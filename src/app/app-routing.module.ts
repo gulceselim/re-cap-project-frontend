01f5c9dbda3aeecfc4c8177eded5f6d68d2911d2
gulceselim <selimgulce@hotmail.com>
2021-03-12 20:01:48 +0300

    Some files about the project have been added✔

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
+import { UserComponent } from './components/user/user.component';
+import { RentComponent } from './components/rent/rent.component';
+import { ColorComponent } from './components/color/color.component';
+import { CarComponent } from './components/car/car.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BrandComponent } from './components/brand/brand.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'brand',
+    component: BrandComponent,
+  },
+  {
+    path: 'car',
+    component: CarComponent,
+  },
+  {
+    path: 'color',
+    component: ColorComponent,
+  },
+  {
+    path: 'rent',
+    component: RentComponent,
+  },
+  {
+    path: 'user',
+    component: UserComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,40 @@
+import { MaterialModule } from './components/material/material.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { ColorComponent } from './components/color/color.component';
+import { UserComponent } from './components/user/user.component';
+import { CarComponent } from './components/car/car.component';
+import { RentComponent } from './components/rent/rent.component';
+import { NavComponent } from './components/nav/nav.component';
+import { CategoryComponent } from './components/category/category.component';
+import { ColorDialogComponent } from './components/color-dialog/color-dialog.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    BrandComponent,
+    ColorComponent,
+    UserComponent,
+    CarComponent,
+    RentComponent,
+    NavComponent,
+    CategoryComponent,
+    ColorDialogComponent,
+  ],
+  entryComponents: [ColorDialogComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
