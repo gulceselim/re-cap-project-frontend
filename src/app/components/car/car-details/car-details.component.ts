b07e817ee143ad32a7cc2fe535fffaee150d6145
gulceselim <selimgulce@hotmail.com>
2021-03-15 22:15:48 +0300

    car-details component has been added✔

--- src/app/components/car/car-details/car-details.component.ts
+++ src/app/components/car/car-details/car-details.component.ts
@@ -18,12 +18,30 @@ export class CarDetailsComponent implements OnInit {
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe((params) => {
-      this.getCarDetails(params['carId']);
+      if (params['colorId']) {
+        this.getCarsByColor(params['colorId']);
+      } else if (params['brandId']) {
+        this.getCarsByBrand(params['brandId']);
+      } else {
+        this.getCarDetails(params['carId']);
+      }
     });
   }
 
   getCarDetails(carId: number) {
     this.carService.getCarsDetails(carId).subscribe((response) => {
+      this.cars = response.data;
+    });
+  }
+
+  getCarsByColor(colorId: number) {
+    this.carService.getCarsByColor(colorId).subscribe((response) => {
+      this.cars = response.data;
+    });
+  }
+
+  getCarsByBrand(brandId: number) {
+    this.carService.getCarsByBrand(brandId).subscribe((response) => {
       this.cars = response.data;
     });
   }
