aef632466b7ac01e06fe662b02a8e17743d13433
gulceselim <selimgulce@hotmail.com>
2021-03-15 21:20:19 +0300

    carDetails model has been added✔

---
+++ src/app/models/carDetails.ts
@@ -0,0 +1,10 @@
+export interface CarDetails {
+  carId: number;
+  modelYear: string;
+  dailyPrice: number;
+  descriptions: string;
+  brandName: string;
+  colorName: string;
+  carImageDate: string;
+  imagePath: string;
+}
