dd7f6dd694ca20b801759a5752a011de5e2c0f07
gulceselim <selimgulce@hotmail.com>
2021-04-07 15:46:49 +0300

    Navbar component has been added✔

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,15 +1,48 @@
+import { UserService } from './../../services/user.service';
+import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
+  fullName: string;
+  userId: number;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
+    if (this.controlAuthentication() == true) {
+      this.fullName = this.getFullName();
+      this.getUser();
+    }
   }
 
+  controlAuthentication(): boolean {
+    return this.authService.isAuthenticated();
+  }
+
+  getFullName(): string {
+    return localStorage.getItem('fullName');
+  }
+
+  getUser(): number {
+    this.userService.getUsers().subscribe((response) => {
+      response.data.forEach((user) => {
+        if (user.email == this.getFullName()) {
+          this.userId = user.id;
+        }
+      });
+    });
+    return this.userId;
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 }
